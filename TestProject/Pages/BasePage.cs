af542b745ed3d55eb834a70f1eb7a2ce9a709319
DaniilKolodiy <kolodiy.danil@gmail.com>
2020-12-16 12:50:02 +0200

    Add files via upload

---
+++ TestProject/Pages/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TestProject.Pages
+{
+    public class BasePage
+    {
+        protected IWebDriver Driver { get; set; }
+        public BasePage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public WebDriverWait Wait()
+        {
+            return new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+        }
+
+    }
+}
