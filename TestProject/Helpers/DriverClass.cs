32807665717f0a251caa1ee7a33fec3a89238255
DaniilKolodiy <kolodiy.danil@gmail.com>
2020-12-16 12:49:12 +0200

    Add files via upload

---
+++ TestProject/Helpers/DriverClass.cs
@@ -0,0 +1,55 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace TestProject.Helpers
+{
+    public class DriversClass
+    {
+        public IWebDriver Create(BrowserType browserType)
+        {
+            switch (browserType)
+            {
+                case BrowserType.Chrome:
+                    return GetChromeDriver();
+                case BrowserType.Firefox:
+                    return GetFirefoxDriver();
+                case BrowserType.Edge:
+                    return GetEdgeDriver();
+                default:
+                    throw new ArgumentOutOfRangeException("No such browser exists");
+            }
+        }
+
+        private IWebDriver GetEdgeDriver()
+        {
+            EdgeOptions edgeOptions = new EdgeOptions();
+            edgeOptions.UseChromium = true;
+            edgeOptions.AddArguments("--start-maximized");
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new EdgeDriver(outPutDirectory, edgeOptions);
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--start-maximized");
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory, chromeOptions);
+        }
+
+        private IWebDriver GetFirefoxDriver()
+        {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AddArgument("--start-maximized");
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new FirefoxDriver(outPutDirectory, firefoxOptions);
+        }
+
+    }
+
+}
